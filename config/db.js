ece1a11422d86a16ab53d0bbc47756f72d6c82d3
asfakwork07 <asfak.work07@gmail.com>
2026-07-28 00:12:56 +0530

    Backend ready for deployment

--- config/db.js
+++ config/db.js
@@ -1,16 +1,26 @@
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
+// pool
+//   .connect()
+//   .then(() => console.log("PostgreSQL Connected"))
+//   .catch((err) => console.log(err));
+
+// module.exports = pool;
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-
-pool
-  .connect()
-  .then(() => console.log("PostgreSQL Connected"))
-  .catch((err) => console.log(err));
 
 module.exports = pool;
